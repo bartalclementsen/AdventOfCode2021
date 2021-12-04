ecd461858a90ebca480f09e4a9516c9c1d1f6c35
Bartal Clementsen <bartalclementsen@hotmail.com>
2021-12-04 12:23:12 +0000

    Day 4 solved

---
+++ src/AdventOfCode2021.Day4/DayUnitTest1.cs
@@ -0,0 +1,117 @@
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2021.Day4
+{
+
+    public class DayUnitTest1
+    {
+        private readonly ITestOutputHelper output;
+
+        public DayUnitTest1(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void Star1_Test_1()
+        {
+            // Arrange
+            string input =
+@"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7";
+
+            Solver solver = new();
+
+            // Act
+            var result = solver.SolveDayStar1(input);
+
+            // Assert
+            Assert.Equal("4512", result);
+        }
+
+        [Fact]
+        public void Star1_Solve()
+        {
+            // Arrange
+            string input = File.ReadAllText("data1.txt");
+
+            Solver solver = new();
+
+            // Act
+            var result = solver.SolveDayStar1(input);
+
+            // Assert
+            output.WriteLine(result);
+        }
+
+
+
+        [Fact]
+        public void Star2_Test_1()
+        {
+            // Arrange
+            string input =
+@"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7";
+
+            Solver solver = new();
+
+            // Act
+            var result = solver.SolveDayStar2(input);
+
+            // Assert
+            Assert.Equal("1924", result);
+        }
+
+        [Fact]
+        public void Star2_Solve()
+        {
+            // Arrange
+            string input = File.ReadAllText("data1.txt");
+
+            Solver solver = new();
+
+            // Act
+            var result = solver.SolveDayStar2(input);
+
+            // Assert
+            output.WriteLine(result);
+        }
+    }
+}
---
+++ src/AdventOfCode2021.Day4/Solver.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2021.Day4
+{
+    internal class Solver
+    {
+        public string SolveDayStar1(string input)
+        {
+            var bingoGame = new BingoGame(input);
+
+            while (bingoGame.WinnerFound == false)
+            {
+                bingoGame.DrawNextNumber();
+            }
+
+            int? index = bingoGame?.GetWinningHash();
+
+
+            return index?.ToString() ?? "UNKNOWN";
+        }
+
+        public string SolveDayStar2(string input)
+        {
+            var bingoGame = new BingoGame(input);
+
+            while (bingoGame.AreAllWinnersFound == false)
+            {
+                bingoGame.DrawNextNumber();
+            }
+
+            int? index = bingoGame?.GetLastWinningHash();
+
+
+            return index?.ToString() ?? "UNKNOWN";
+        }
+
+        public class BingoGame
+        {
+            public bool WinnerFound => _winners.Any();
+
+            public bool AreAllWinnersFound => _playingBingoBoards.Any() == false;
+
+
+            private int _bingoNumberIndex = -1;
+
+            private readonly List<int> _bingoNumbers;
+            private readonly List<BingoBoard> _bingoBoards = new();
+            private readonly List<BingoBoard> _playingBingoBoards = new();
+            private readonly List<BingoBoard> _winners = new();
+
+            public BingoGame(string input)
+            {
+                IEnumerable<string>? bingoInput = input.SplitByNewLine();
+                _bingoNumbers = bingoInput.ElementAt(0).Split(",").Select(bn => int.Parse(bn)).ToList();
+
+                for (int i = 2; i < bingoInput.Count(); i += 6)
+                {
+                    _bingoBoards.Add(new BingoBoard(bingoInput.Skip(i).Take(5)));
+                }
+                _playingBingoBoards.AddRange(_bingoBoards);
+            }
+
+            public override string ToString()
+            {
+                StringBuilder sb = new();
+
+                sb.AppendLine(string.Join(",", _bingoNumbers));
+
+                foreach (var board in _playingBingoBoards)
+                {
+                    sb.AppendLine();
+                    sb.AppendLine(board.ToString());
+                }
+
+                return sb.ToString();
+            }
+
+            public void DrawNextNumber()
+            {
+                _bingoNumberIndex++;
+                if (_bingoNumberIndex > _bingoNumbers.Count - 1)
+                    throw new IndexOutOfRangeException("Can not draw more numbers");
+
+                int currentNumber = GetCurrentNumeber();
+
+                foreach (var board in _playingBingoBoards.ToList())
+                {
+                    board.PlayNumber(currentNumber);
+                    if (board.HasWon)
+                    {
+                        _winners.Add(board);
+                        _playingBingoBoards.Remove(board);
+                    }
+                }
+            }
+
+            public int? GetWinningHash() => CalulateWinningHash(GetWinningBoard());
+
+            public int? GetLastWinningHash() => CalulateWinningHash(GeLastWinningBoard());
+
+            private int? CalulateWinningHash(BingoBoard? bingoBoard) => bingoBoard?.GetSumOfUnmarkedNumbers() * GetCurrentNumeber();
+
+            private int GetCurrentNumeber() => _bingoNumbers[_bingoNumberIndex];
+
+            private BingoBoard? GetWinningBoard() => _winners.FirstOrDefault();
+
+            private BingoBoard? GeLastWinningBoard() => _winners.LastOrDefault();
+        }
+
+        public class BingoBoard
+        {
+            public bool HasWon
+            {
+                get
+                {
+                    if (_rows.Any(r => r.HasWon))
+                        return true;
+
+                    for (int i = 0; i < 5; i++)
+                    {
+                        bool columnWon = true;
+
+                        for (int j = 0; j < 5; j++)
+                        {
+                            if (_rows[j].Numbers[i].IsChecked == false)
+                            {
+                                columnWon = false;
+                                break;
+                            }
+                        }
+
+
+                        if (columnWon)
+                            return true;
+                    }
+
+
+                    return false;
+                }
+            }
+
+            private readonly List<BinogRow> _rows;
+
+            public BingoBoard(IEnumerable<string> rows)
+            {
+                _rows = rows.Select(r => new BinogRow(r)).ToList();
+            }
+
+            internal int GetSumOfUnmarkedNumbers()
+            {
+                return _rows.SelectMany(b => b.Numbers).Where(n => n.IsChecked == false).Sum(o => o.Number);
+            }
+
+            internal void PlayNumber(int currentNumber)
+            {
+                foreach (var row in _rows)
+                {
+                    if (row.TryPlayNumber(currentNumber))
+                        break;
+                }
+            }
+
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var row in _rows)
+                {
+                    sb.AppendLine(row.ToString());
+                }
+                return sb.ToString();
+            }
+        }
+
+        public class BinogRow
+        {
+            public bool HasWon => Numbers.All(n => n.IsChecked);
+
+            public List<BingoNumber> Numbers { get; }
+
+            public BinogRow(string numbers)
+            {
+                Numbers = Regex.Matches(numbers, @"\d+").Select(m => new BingoNumber(m.Value)).ToList();
+            }
+
+            internal bool TryPlayNumber(int number)
+            {
+                var foundNumber = Numbers.FirstOrDefault(n => n.Number == number);
+                if (foundNumber != null)
+                {
+                    foundNumber.Check();
+                    return true;
+                }
+
+                return false;
+            }
+
+            public override string ToString()
+            {
+                return string.Join(" ", Numbers);
+            }
+        }
+
+        public class BingoNumber
+        {
+            public int Number { get; }
+
+            public bool IsChecked { get; private set; }
+
+            public BingoNumber(string number)
+            {
+                Number = int.Parse(number);
+            }
+
+            public void Check()
+            {
+                IsChecked = true;
+            }
+
+            public override string ToString()
+            {
+                string value = Number.ToString("00");
+                value += IsChecked ? "X" : "O";
+                return value;
+            }
+        }
+    }
+}
